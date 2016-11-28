3e7e79b7efeb61a858d07dc71ae2b2c60dd260c1
韦思聪 <weisicong@weisicongdemacbook-pro.local>
2016-11-28 22:14:25 +0800

    Initial Commit

---
+++ B-spline/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  B-spline
+//
+//  Created by 韦思聪 on 16/11/28.
+//  Copyright © 2016年 Wei sicong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
